288c850efd1ad4b24b95857d56b80cf13e0ce820
Roman Wernsing <wernsingroman@gmail.com>
2025-02-18 16:28:02 -0700

    added more buttons, everything is working great

--- app.js
+++ app.js
@@ -2,18 +2,48 @@
 
 const menuItems = [
   {
-    name: 'sprinkles',
+    name: 'Sprinkles',
     price: 2,
     quantity: 0
   },
   {
-    name: 'chocolate chip',
+    name: 'Chocolate Chip',
     price: 3,
     quantity: 0,
   },
   {
-    name: 'gummy worms',
+    name: 'Gummy Worms',
     price: 2,
+    quantity: 0,
+  },
+  {
+    name: 'Waffle Cone',
+    price: 3,
+    quantity: 0,
+  },
+  {
+    name: 'Waffle Bowl',
+    price: 4,
+    quantity: 0,
+  },
+  {
+    name: 'Dipped Cone',
+    price: 5,
+    quantity: 0,
+  },
+  {
+    name: 'Vanilla Ice Cream',
+    price: 3,
+    quantity: 0,
+  },
+  {
+    name: 'Chocolate Ice Cream',
+    price: 4,
+    quantity: 0,
+  },
+  {
+    name: 'Strawberry Ice Cream',
+    price: 4,
     quantity: 0,
   },
 ]
@@ -31,7 +61,39 @@ function addItemToCart(indexNumber) {
 
   drawCartName()
   drawCartQuantity()
+  drawCartPrice()
+  drawCartTotal()
+  drawFullCartTotal()
 
+}
+
+function calculateCartTotal() {
+
+  let total = 0
+
+  for (let i = 0; i < menuItems.length; i++) {
+    const menuItem = menuItems[i]
+    total += menuItem.price * menuItem.quantity
+  }
+
+  return total
+
+}
+
+function checkout() {
+  const total = calculateCartTotal()
+
+  if (total == 0) {
+    window.alert("Please add something to your cart!!! 🤬")
+  }
+
+  window.alert(`Your order has been confirmed. your total was $${total}!`)
+
+  drawCartName()
+  drawCartQuantity()
+  drawCartPrice()
+  drawCartTotal()
+  drawFullCartTotal()
 }
 
 // ANCHOR drawing (DOM manipulation)
@@ -68,19 +130,45 @@ function drawCartQuantity() {
   quantityListElem.innerHTML = itemNames
 
 }
-function drawCartQuantity() {
+function drawCartPrice() {
   let itemNames = ''
 
   for (let i = 0; i < menuItems.length; i++) {
     const menuItem = menuItems[i]
 
     if (menuItem.quantity > 0) {
-      itemNames += `<p>${menuItem.quantity}<p/>`
+      itemNames += `<p>$${menuItem.price}<p/>`
     }
 
   }
 
-  const quantityListElem = document.getElementById('quantityList')
-  quantityListElem.innerHTML = itemNames
+  const priceListElem = document.getElementById('priceList')
+  priceListElem.innerHTML = itemNames
 
 }
+function drawCartTotal() {
+  let itemNames = ''
+
+  for (let i = 0; i < menuItems.length; i++) {
+    const menuItem = menuItems[i]
+
+    if (menuItem.quantity > 0) {
+      itemNames += `<p>$${menuItem.price * menuItem.quantity}<p/>`
+    }
+
+  }
+
+  const totalListElem = document.getElementById('totalList')
+  totalListElem.innerHTML = itemNames
+
+}
+
+function drawFullCartTotal() {
+  const cartTotalElem = document.getElementById('fullTotal')
+
+  const cartTotal = calculateCartTotal()
+
+  cartTotalElem.innerText = '$' + cartTotal
+
+}
+
