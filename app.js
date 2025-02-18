ad3536aa1ac3a2b31bbf54098307b6ba2cd4e10c
Roman Wernsing <wernsingroman@gmail.com>
2025-02-18 15:26:35 -0700

    item name and quantity shows up on shopping cart

--- app.js
+++ app.js
@@ -21,7 +21,66 @@ const menuItems = [
 // ANCHOR action / logic
 
 function addItemToCart(indexNumber) {
-  const menueItem = menuItems[indexNumber]
+  const menuItem = menuItems[indexNumber]
+
+  if (menuItem == undefined) {
+    console.warn(`${indexNumber} is not a valid index`)
+  }
+
+  menuItem.quantity++
+
+  drawCartName()
+  drawCartQuantity()
+
 }
 
 // ANCHOR drawing (DOM manipulation)
+
+function drawCartName() {
+  let itemNames = ''
+
+  for (let i = 0; i < menuItems.length; i++) {
+    const menuItem = menuItems[i]
+
+    if (menuItem.quantity > 0) {
+      itemNames += `<p>${menuItem.name}<p/>`
+    }
+
+  }
+
+  const nameListElem = document.getElementById('nameList')
+  nameListElem.innerHTML = itemNames
+
+}
+function drawCartQuantity() {
+  let itemNames = ''
+
+  for (let i = 0; i < menuItems.length; i++) {
+    const menuItem = menuItems[i]
+
+    if (menuItem.quantity > 0) {
+      itemNames += `<p>${menuItem.quantity}<p/>`
+    }
+
+  }
+
+  const quantityListElem = document.getElementById('quantityList')
+  quantityListElem.innerHTML = itemNames
+
+}
+function drawCartQuantity() {
+  let itemNames = ''
+
+  for (let i = 0; i < menuItems.length; i++) {
+    const menuItem = menuItems[i]
+
+    if (menuItem.quantity > 0) {
+      itemNames += `<p>${menuItem.quantity}<p/>`
+    }
+
+  }
+
+  const quantityListElem = document.getElementById('quantityList')
+  quantityListElem.innerHTML = itemNames
+
+}
