d46642ae7e5e1040d651b5f6635a642070202dac
Roman Wernsing <wernsingroman@gmail.com>
2025-02-18 14:33:52 -0700

    rough html draft, made an array with objects in it

---
+++ app.js
@@ -0,0 +1,27 @@
+// ANCHOR global variables
+
+const menuItems = [
+  {
+    name: 'sprinkles',
+    price: 2,
+    quantity: 0
+  },
+  {
+    name: 'chocolate chip',
+    price: 3,
+    quantity: 0,
+  },
+  {
+    name: 'gummy worms',
+    price: 2,
+    quantity: 0,
+  },
+]
+
+// ANCHOR action / logic
+
+function addItemToCart(indexNumber) {
+  const menueItem = menuItems[indexNumber]
+}
+
+// ANCHOR drawing (DOM manipulation)
